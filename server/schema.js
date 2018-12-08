e1510804a70da7deb8719894711a1446b41746fa
Devonte M <devontem@gmail.com>
2018-12-08 13:33:11 -0800

    updated mock_db with more accurate information and refactored deleted method for deletePosition resolver

--- server/schema.js
+++ server/schema.js
@@ -2,25 +2,56 @@ const { gql } = require("apollo-server-express");
 
 // mock data
 const positions = [
-  { role: "sdet", description: "fun", year: "2009", company: "yahoo" },
   {
-    role: "sdet",
-    description: "great",
-    year: "2014",
-    company: "music reports"
+    role: "Software Engineer",
+    description:
+      "Implemented features within the Evite chat messaging system using ReactJS, Redux, and Python",
+    year: "2018",
+    company: "Evite"
   },
   {
-    role: "sdet",
-    description: "exciting",
-    year: "2021",
-    company: "test3"
+    role: "Software Engineer",
+    description:
+      "Integrated 2017-2018 Live Streaming NFL Playoff games on Yahoo homepage, reaching 600+m monthly users",
+    year: "2017",
+    company: "Yahoo!"
+  },
+  {
+    role: "Software Engineer",
+    description:
+      "Implemented features in Self Serve (manage account, payment, etc.) used by Koodo subscribers",
+    year: "2016",
+    company: "Koodo"
+  },
+  {
+    role: "Software Developer",
+    description:
+      "Created and maintained multiple internal web applications using HTML, CSS, & Javascript for a Music Industry CMS",
+    year: "2015",
+    company: "Music Reports"
+  },
+  {
+    role: "Software Developer",
+    description:
+      "Developed web applications using HTML, CSS, & Javascript for a variety of clients that interacted with a custom-built CMS system using ColdFusion ",
+    year: "2015",
+    company: "Creative Engine"
+  },
+  {
+    role: "Software Developer",
+    description:
+      "Developed pixel-perfect web applications using HTML, CSS, & Javascript based on PSD mock-ups",
+    year: "2013",
+    company: "Sly & Swift"
   }
 ];
 const resume = {
   name: "Devonte MacGlashan",
   city: "Los Angeles",
+  photo_url:
+    "https://avatars3.githubusercontent.com/u/3604690?s=400&u=a8842bd304ea442fa8f60e3c312294f3e14559ae&v=4",
   positions: positions,
-  github_handle: "devontem"
+  github_url: "https://github.com/devontem"
 };
 const mock_db = {
   resume: resume,
@@ -61,6 +92,7 @@ const typeDefs = gql`
   }
 `;
 
+// resolvers
 const resolvers = {
   Query: {
     position: (root, args, { req }, info) => {
@@ -76,8 +108,8 @@ const resolvers = {
       return mock_db.positions[mock_db.positions.length - 1];
     },
     deletePosition: (root, args, { req, res }, info) => {
-      const position = mock_db.positions.find((obj, idx) => idx == args.idx);
-      mock_db.positions = mock_db.positions.filter(obj => obj.id != args.id);
+      const position = mock_db.positions[args.idx];
+      mock_db.positions.splice(args.idx, 1);
       return position;
     }
   }
