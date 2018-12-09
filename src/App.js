614746e31b16e4814fa69d592dbbbebdea849478
Devonte M <devontem@gmail.com>
2018-12-08 17:37:15 -0800

    added Apollo Client to react App

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+import "./App.css";
+import Resume from "./components/resume/Resume";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql"
+});
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ApolloProvider client={client}>
+        <div>
+          <header>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+              <a className="navbar-brand" href="/">
+                Devonte - Tron
+              </a>
+            </nav>
+          </header>
+          <div className="container">
+            <Resume />
+          </div>
+        </div>
+      </ApolloProvider>
     );
   }
 }
