186aceb6176708c8aa2518491d0ae5769d32f205
Devonte M <devontem@gmail.com>
2018-12-09 22:59:09 -0800

    added functionality to render react app from root and serve static files

--- server/server.js
+++ server/server.js
@@ -1,12 +1,23 @@
-const express = require('express');
-const { ApolloServer, gql } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schema');
+const express = require("express");
+const path = require("path");
+const { ApolloServer, gql } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./schema");
 
+// apollo server
 const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 server.applyMiddleware({ app });
 
+// serving static files
+app.use(express.static(path.resolve(__dirname, "..", "build")));
+
+// render react app
+app.get("/", function(req, res) {
+	res.sendFile(path.resolve(__dirname, "..", "build", "index.html"));
+});
+
+// start server
 app.listen({ port: 4000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+	console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
 );
