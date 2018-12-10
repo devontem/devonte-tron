6465dac287e5456927144efecfa541d461578866
Devonte M <devontem@gmail.com>
2018-12-09 22:55:52 -0800

    refactored spacing to spaces 2 tabs

--- src/components/resume/resume.js
+++ src/components/resume/resume.js
@@ -1,1 +1,33 @@
-resume.js;
+import React, { Component } from "react";
+import { Query } from "react-apollo";
+
+import ResumeData from "./ResumeData";
+import PositionList from "./../position/PositionList";
+import { GET_RESUME } from "./../../graphql/statements";
+
+class Resume extends Component {
+  render() {
+    return (
+      <Query query={GET_RESUME}>
+        {({ loading, error, data }) => {
+          if (loading) return "Loading...";
+          if (error) return `Error! ${error.message}`;
+
+          const resume_props = data.resume ? data.resume : {};
+
+          return (
+            <div className="row" style={{ marginTop: "20px" }}>
+              <ResumeData {...resume_props} />
+
+              <PositionList
+                positions={data.resume ? data.resume.positions : []}
+              />
+            </div>
+          );
+        }}
+      </Query>
+    );
+  }
+}
+
+export default Resume;
